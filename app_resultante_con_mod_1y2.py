853b8fa0a7bfc97adea3ec135923956eb73388e6
florcuevasgithub <110203715+florcuevasgithub@users.noreply.github.com>
2025-05-27 15:47:16 -0300

    Update app_resultante_con_mod_1y2.py

--- app_resultante_con_mod_1y2.py
+++ app_resultante_con_mod_1y2.py
@@ -317,16 +317,16 @@ if opcion == "1️⃣ Análisis de una medición":
                 st.warning("No se encontraron datos suficientes para el análisis.")
 
 # Función para reiniciar archivos
-        def reset_archivos():
-            for key in ["reposo_file", "postural_file", "accion_file"]:
+       def reset_archivos():
+            for key in ["reposo_file", "postural_file", "accion_file",
+                        "reposo_file_1", "postural_file_1", "accion_file_1",
+                        "reposo_file_2", "postural_file_2", "accion_file_2"]:
                 if key in st.session_state:
                     del st.session_state[key]
 
 # Botón de reinicio
         if st.button("🔄 Nuevo análisis"):
             reset_archivos()
-            if "resultados_globales" in st.session_state:
-                del st.session_state.resultados_globales
             st.experimental_rerun()
 
 
